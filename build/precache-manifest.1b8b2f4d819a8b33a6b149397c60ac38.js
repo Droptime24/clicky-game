b4b44c2b7019dd5d2665efb8e3bd2933407c6eb0
Droptime24 <you@example.com>
2019-10-02 16:13:25 -0600

    deploy try 3

---
+++ build/precache-manifest.1b8b2f4d819a8b33a6b149397c60ac38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947845996ace3a5c43add67d8bb1fbc4",
+    "url": "/Droptime24/clicky-game/index.html"
+  },
+  {
+    "revision": "cf49332f089994f098c0",
+    "url": "/Droptime24/clicky-game/static/css/main.96f9654e.chunk.css"
+  },
+  {
+    "revision": "f1bc12b69bd327c1bb2e",
+    "url": "/Droptime24/clicky-game/static/js/2.04b803ad.chunk.js"
+  },
+  {
+    "revision": "cf49332f089994f098c0",
+    "url": "/Droptime24/clicky-game/static/js/main.cc20bfb5.chunk.js"
+  },
+  {
+    "revision": "e0176114de0dc90de446",
+    "url": "/Droptime24/clicky-game/static/js/runtime-main.3c44a120.js"
+  }
+]);
